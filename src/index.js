7386c6b289abd2fd290524067b5db3bcd1e3f840
anubhav <anubhavshivahre@gmail.com>
2023-01-23 14:15:38 +0530

    added instructions in readme file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig.js');
+const bodyParser = require('body-parser');
+
 const createAndStartServer = async () => {
+    //create the express object
     const app = express();
+
+    //setting up middleware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
 
     app.listen(PORT, () => {
         console.log(`server started on ${PORT}`);
-
+        
     });
 };
 
