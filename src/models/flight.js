9e91dee929cdf6261357c7ee2fa46a837c244021
anubhav <anubhavshivahre@gmail.com>
2023-01-31 21:32:57 +0530

    added flights model

---
+++ src/models/flight.js
@@ -0,0 +1,59 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class flight extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  flight.init({
+    flightNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique:true
+    },
+    airplaneId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    departureAiportId:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    arrivalAirportId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    departureTime:{
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    arrivalTime: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    price:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    boardingGate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    totalSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'flight',
+  });
+  return flight;
+};
